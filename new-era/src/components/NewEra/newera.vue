4dbb2bf75c25ee34b98cd16062140678de02cb91
lexiaoyuan <31907363+lexiaoyuan@users.noreply.github.com>
2019-06-12 17:27:18 +0800

    开始写新时代页面

---
+++ new-era/src/components/NewEra/newera.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav-bar></nav-bar>
+    </div>
+</template>
+<script>
+import NavBar from '../NavBar'
+export default {
+   components: {
+       NavBar
+   }
+    
+}
+</script>
+<style lang="">
+    
+</style>
