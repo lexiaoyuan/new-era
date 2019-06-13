6cc44000aeb66a5eb2d1a7d370c3a5c2303f2dac
lexiaoyuan <31907363+lexiaoyuan@users.noreply.github.com>
2019-06-13 13:34:11 +0800

    修改新时代页面图片尺寸要求

--- new-era/src/components/NewEra/NewEraOneMinuteVideo.vue
+++ new-era/src/components/NewEra/NewEraOneMinuteVideo.vue
@@ -1,35 +1,109 @@
 <template>
   <div>
-    <b-container fluid class="bv-example-row">
-      <b-row align-h="center">
-        <b-col cols="4" sm="4">
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
+    <div class="d-flex justify-content-sm-between mt-5 pl-3 pr-5">
+      <hr class="line-left"/>
+      <h3>中国一分钟</h3>
+      <hr class="w-75"/>
+      <h5>更多</h5>
+    </div>
+    <b-container fluid>
+      <b-row align-h="around">
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[0].title"
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')" 
+            img-alt="Image" 
+            img-top
+            tag="article" 
+            style="max-width: 23rem;"
+            class="mb-2"
+            >
             <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
+              {{ video[0].brief }}
             </b-card-text>
 
-            <b-button href="#" variant="primary">Go somewhere</b-button>
+            <b-button href="#" variant="primary">查看详情</b-button>
           </b-card>
         </b-col>
-        <b-col cols="4" sm="4">
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[1].title" 
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')"
+            img-alt="Image" 
+            img-top
+            tag="article"
+            style="max-width: 23rem;" 
+            class="mb-2">
             <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
+              {{ video[1].brief }}
             </b-card-text>
 
-            <b-button href="#" variant="primary">Go somewhere</b-button>
+            <b-button href="#" variant="primary">查看详情</b-button>
           </b-card>
         </b-col>
-        <b-col cols="4" sm="4">
-          <b-card title="Card Title" img-src="https://picsum.photos/600/300/?image=25" img-alt="Image" img-top
-            tag="article" style="max-width: 20rem;" class="mb-2">
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[2].title" 
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')" 
+            img-alt="Image" 
+            img-top
+            tag="article" 
+            style="max-width: 23rem;"
+            class="mb-2">
             <b-card-text>
-              Some quick example text to build on the card title and make up the bulk of the card's content.
+              {{ video[2].brief }}
             </b-card-text>
 
-            <b-button href="#" variant="primary">Go somewhere</b-button>
+            <b-button href="#" variant="primary">查看详情</b-button>
+          </b-card>
+        </b-col>
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[0].title"
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')" 
+            img-alt="Image" 
+            img-top
+            tag="article" 
+            style="max-width: 23rem;"
+            class="mb-2"
+            >
+            <b-card-text>
+              {{ video[0].brief }}
+            </b-card-text>
+
+            <b-button href="#" variant="primary">查看详情</b-button>
+          </b-card>
+        </b-col>
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[1].title" 
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')"
+            img-alt="Image" 
+            img-top
+            tag="article"
+            style="max-width: 23rem;" 
+            class="mb-2">
+            <b-card-text>
+              {{ video[1].brief }}
+            </b-card-text>
+
+            <b-button href="#" variant="primary">查看详情</b-button>
+          </b-card>
+        </b-col>
+        <b-col sm="auto" class="mt-3">
+          <b-card 
+            :title="video[2].title" 
+            :img-src="require('./img/newera-one-minute-video-guangxi.jpg')" 
+            img-alt="Image" 
+            img-top
+            tag="article" 
+            style="max-width: 23rem;"
+            class="mb-2">
+            <b-card-text>
+              {{ video[2].brief }}
+            </b-card-text>
+
+            <b-button href="#" variant="primary">查看详情</b-button>
           </b-card>
         </b-col>
       </b-row>
@@ -38,10 +112,33 @@
 </template>
 <script>
   export default {
-
+    data () {
+      return {
+        video: [
+          {
+            title: '广西一分钟',
+            brief: '关于广西一分钟的简短描述，关于广西一分钟的简短描述，关于广西一分钟的简短描述，',
+            link: 'http://.....'
+          },
+          {
+            title: '广西一分钟',
+            brief: '关于广西一分钟的简短描述，关于广西一分钟的简短描述，关于广西一分钟的简短描述，',
+            link: 'http://.....'
+          },
+          {
+            title: '广西一分钟',
+            brief: '关于广西一分钟的简短描述，关于广西一分钟的简短描述，关于广西一分钟的简短描述，',
+            link: 'http://.....'
+          }
+        ]
+      }
+    }
   }
 
 </script>
 <style scoped>
+  .line-left {
+    width: 2rem !important;
+  }
 
 </style>
