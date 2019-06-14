22eb592fbf5e1d13f9d056d16f012c286ce71251
lexiaoyuan <31907363+lexiaoyuan@users.noreply.github.com>
2019-06-14 17:20:19 +0800

    创建完成相关组件

--- new-era/src/views/NewEra.vue
+++ new-era/src/views/NewEra.vue
@@ -1,31 +1,32 @@
 <template>
-    <div>
-        <nav-bar></nav-bar>
-        <new-era-one-minute-brief></new-era-one-minute-brief>
-        <new-era-one-minute-video></new-era-one-minute-video>
-        <new-era-achieve></new-era-achieve>
-        <new-era-call></new-era-call>
-        <bottom-declare></bottom-declare>
-        <router-view></router-view>
-    </div>
+  <div>
+    <nav-bar></nav-bar>
+    <new-era-one-minute-brief></new-era-one-minute-brief>
+    <new-era-one-minute-video></new-era-one-minute-video>
+    <new-era-achieve></new-era-achieve>
+    <new-era-call></new-era-call>
+    <bottom-declare></bottom-declare>
+    <router-view></router-view>
+  </div>
 </template>
 <script>
-import NavBar from '../components/NavBar'
-import NewEraOneMinuteBrief from '../components/NewEra/NewEraOneMinuteBrief'
-import NewEraOneMinuteVideo from '../components/NewEra/NewEraOneMinuteVideo'
-import NewEraAchieve from '../components/NewEra/NewEraAchieve'
-import NewEraCall from '../components/NewEra/NewEraCall'
-import BottomDeclare from '../components/BottomDeclare'
-export default {
+  import NavBar from '../components/NavBar'
+  import NewEraOneMinuteBrief from '../components/NewEra/NewEraOneMinuteBrief'
+  import NewEraOneMinuteVideo from '../components/NewEra/NewEraOneMinuteVideo'
+  import NewEraAchieve from '../components/NewEra/NewEraAchieve'
+  import NewEraCall from '../components/NewEra/NewEraCall'
+  import BottomDeclare from '../components/BottomDeclare'
+  export default {
     components: {
-        NavBar,
-        NewEraOneMinuteBrief,
-        NewEraOneMinuteVideo,
-        NewEraAchieve,
-        NewEraCall,
-        BottomDeclare
+      NavBar,
+      NewEraOneMinuteBrief,
+      NewEraOneMinuteVideo,
+      NewEraAchieve,
+      NewEraCall,
+      BottomDeclare
     }
-}
+  }
+
 </script>
 <style scoped>
 
---
+++ new-era/src/views/NewJourney.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <nav-bar></nav-bar>
+      <p>新征程</p>
+      <router-view></router-view>
+  </div>
+</template>
+<script>
+import NavBar from '../components/NavBar'
+export default {
+    components: {
+        NavBar
+    }
+}
+</script>
+<style scoped>
+
+</style>
