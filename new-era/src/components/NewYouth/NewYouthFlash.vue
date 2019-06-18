22ebd5c844d36de03356ccdef913f4aede6bc8ca
lexiaoyuan <31907363+lexiaoyuan@users.noreply.github.com>
2019-06-18 14:54:14 +0800

    更新部分内容

--- new-era/src/components/NewYouth/NewYouthFlash.vue
+++ new-era/src/components/NewYouth/NewYouthFlash.vue
@@ -1,14 +1,65 @@
 <template>
   <div>
-
+    <b-container fluid>
+      <b-row>
+        <b-col class="ml-4 mr-4 mt-4">
+          <fieldset class="layui-elem-field layui-field-title">
+            <legend>
+              <h3 class="wow animated fadeInUp" data-wow-duration="1s" data-wow-delay="0.5s">青春快闪</h3>
+            </legend>
+          </fieldset>
+        </b-col>
+      </b-row>
+      <b-row align-h="around">
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          <b-img></b-img>
+        </b-col>
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          
+        </b-col>
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          
+        </b-col>
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          
+        </b-col>
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          
+        </b-col>
+        <b-col sm="auto" class="mt-3 wow animated fadeInUp">
+          
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <script>
-export default {
-    
-}
+  import {
+    WOW
+  } from 'wowjs'
+  export default {
+    mounted() {
+      var wow = new WOW({
+        boxClass: 'wow',
+        animateClass: 'animated',
+        offset: 30,
+        mobile: true,
+        live: false
+      })
+      wow.init()
+    }
+  }
+
 </script>
 <style scoped>
+  fieldset {
+    margin: 0 2px;
+  }
+
+  legend {
+    border: 0;
+    width: auto;
+    margin-bottom: 0px;
+  }
 
 </style>
-
--- new-era/src/views/NewYouth.vue
+++ new-era/src/views/NewYouth.vue
@@ -2,16 +2,19 @@
   <div>
     <nav-bar></nav-bar>
     <new-youth-brief></new-youth-brief>
+    <new-youth-flash></new-youth-flash>
     <router-view></router-view>
   </div>
 </template>
 <script>
   import NavBar from '../components/NavBar'
   import NewYouthBrief from '../components/NewYouth/NewYouthBrief'
+  import NewYouthFlash from '../components/NewYouth/NewYouthFlash'
   export default {
     components: {
       NavBar,
-      NewYouthBrief
+      NewYouthBrief,
+      NewYouthFlash
     }
   }
 
