fd6115c5125c622467e06950a885acfd16562a8e
lexiaoyuan <31907363+lexiaoyuan@users.noreply.github.com>
2019-06-20 18:00:50 +0800

    更新logo

--- new-era/src/components/NavBar.vue
+++ new-era/src/components/NavBar.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
     <b-navbar class="app-nav-bar navbar-expand-sm bg-pure-red">
-      <b-navbar-brand href="#">辉煌新时代，追梦新征程</b-navbar-brand>
+      <b-navbar-brand href="#">
+        <img src="../assets/img/logo.png" alt="辉煌新时代 追梦新征程" class="fluid">
+        <!-- 辉煌新时代 追梦新征程 -->
+      </b-navbar-brand>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">首页</b-nav-item>
           <b-nav-item to="newera">新时代</b-nav-item>
-          <b-nav-item to="newjourney" >新征程</b-nav-item>
+          <b-nav-item to="newjourney">新征程</b-nav-item>
           <b-nav-item to="newyouth">新青年</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-form type="light" variant="light">
-            <b-form-input size="sm" class="mr-sm-2" placeholder="搜索..."></b-form-input>
+            <b-form-input id="bdcsMain" size="sm" class="mr-sm-2" placeholder="搜索..."></b-form-input>
             <b-button variant="outline-success" size="sm" class="app-navbar-search-submit my-2 my-sm-0" type="submit">搜索
             </b-button>
           </b-nav-form>
@@ -31,7 +34,7 @@
 
 <script>
   export default {
-    
+
   }
 
 </script>
@@ -40,10 +43,11 @@
   .bg-pure-red {
     background-color: #e70012;
   }
-  
+
   .nav-link {
     color: #f8f9fa !important;
   }
+
   .router-link-exact-active {
     color: #ec6c00 !important;
   }
